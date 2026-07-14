56983e860231d7ab79e0b6238d0850f3ed48e4e7
Karthick-1905 <jskarthick399@gmail.com>
2026-07-14 21:39:58 +0530

    Chnaged The base Path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: "/Portfolio",
   reactStrictMode: true,
   images: {
     unoptimized: true, // Required Since nextjs Image Optimisation requires a live server and github pages only support static pages
